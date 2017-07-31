a36f7d97180d16c81beea829b1ab7df1b011bc3d
Caylin James <caylin@lougeek.com>
2017-07-31 07:57:55 -0400

    Add comments (Finished)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -24,13 +24,15 @@
                     {{ csrf_field() }}
 
                     <div class="form-group">
-                        <textarea name="body" placeholder="Your comment here." class="form-control"> </textarea>
+                        <textarea name="body" placeholder="Your comment here." class="form-control" required> </textarea>
                     </div>
 
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">Add Comment</button>
                     </div>
                 </form>
+
+                @include ('layouts.errors')
             </div>
         </div>
     </div>
